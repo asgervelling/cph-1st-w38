92f7ad231d55f2dfa78f860e8db4fb00b73648bc
Asger Skov Velling <asger.skov.velling@gmail.com>
2021-02-25 14:22:53 +0100

    First commit

---
+++ src/ex3/Teacher.java
@@ -0,0 +1,15 @@
+public class Teacher {
+    /*
+    3.f in the Teacher class, declare the class "Teacher" and add 3 fields: "name", "age", "isFemale",
+    using appropriate data types for each.
+    */
+    String name;
+    int age;
+    boolean isFemale;
+
+    public Teacher(String name, int age, boolean isFemale) {
+        this.name = name;
+        this.age = age;
+        this.isFemale = isFemale;
+    }
+}
